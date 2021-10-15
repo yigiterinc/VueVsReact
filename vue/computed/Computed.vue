7cbe06d633b14e0f76067b06ce92f84584aab753
Yiğit Kemal Erinç <yigit.erinc@ozu.edu.tr>
2021-10-15 21:25:37 +0200

    Add Vue computed

---
+++ vue/computed/Computed.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h3>Yigit's Favorite Cities are:</h3>
+    <p v-for="city in favCities" :key="city">{{ city }}</p>
+    <h3>Yigit's Favorite Cities in US are:</h3>
+    <p v-for="town in favCitiesInUS" :key="town">{{ town }}</p>
+    <button @click="addBostonToFavCities">
+      Why is Boston not in there? Click to add
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    favCitiesInUS: function () {
+      return this.favCities.filter((city) => this.usCities.includes(city))
+    },
+  },
+  data() {
+    return {
+      favCities: [
+        'Istanbul',
+        'München',
+        'Los Angeles',
+        'Rome',
+        'Florence',
+        'London',
+        'San Francisco',
+      ],
+      usCities: [
+        'New York',
+        'Los Angeles',
+        'Chicago',
+        'Houston',
+        'Phoenix',
+        'Arizona',
+        'San Francisco',
+        'Boston',
+      ],
+    }
+  },
+  methods: {
+    addBostonToFavCities() {
+      if (this.favCities.includes('Boston')) return
+
+      this.favCities.push('Boston')
+    },
+  },
+}
+</script>
+
+<style></style>
